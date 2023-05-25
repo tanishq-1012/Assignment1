37fd62d14d666ffebc4751e8b314cd7faef41bce
tanishq-1012 <tanishq1012@yahoo.com>
2023-05-25 20:24:48 +0530

    Assignment-1

---
+++ Question-1/src/Launch1.java
@@ -0,0 +1,90 @@
+
+public class Launch1 {
+
+	public static void main(String [ ] args) {
+	     int n=9;
+	        for (int i = 0; i < n; i++) {
+
+	            //I
+	            for (int j = 0; j < n; j++) {
+	                if (i==0||i==n-1||j==n/2) 
+	                System.out.print("*");
+	                
+	                else
+	                System.out.print(" ");
+	            }
+	            System.out.print("    ");
+
+
+	            //N
+	            for (int j = 0; j < n; j++) {
+	                if (j==0||j==n-1||i==j) 
+	                System.out.print("*");
+	                
+	                else
+	                System.out.print(" ");
+	            }
+	            System.out.print("    ");
+
+
+	            //E
+	            for (int j = 0; j < n; j++) {
+	                if (j==0||i==0||i==n/2||i==n-1) 
+	                System.out.print("*");
+	                
+	                else
+	                System.out.print(" ");
+	            }
+	            System.out.print("    ");
+
+
+	            //U
+	            for (int j = 0; j < n; j++) {
+	                if (j==0&&i<n-1||j==n-1&&i<n-1||i==n-1&&j>0&&j<n-1) 
+	                System.out.print("*");
+	                
+	                else
+	                System.out.print(" ");
+	            }
+	            System.out.print("    ");
+	  
+	            
+	            //R
+	            for (int j = 0; j < n; j++) {
+	                if (j==0||i==0&&j!=n-1||j==n-1&&i>0&&i<n/2||i==n/2&&j!=n-1||i-j+1==n/2) 
+	                System.out.print("*");
+	                
+	                else
+	                System.out.print(" ");
+	            }
+	            System.out.print("    ");
+
+
+	            //O
+	            for (int j = 0; j < n; j++) {
+	                if (j==0&&i>0&&i<n-1||j==n-1&&i>0&&i<n-1||i==0&&j>0&&j<n-1||i==n-1&&j>0&&j<n-1) 
+	                System.out.print("*");
+	                
+	                else
+	                System.out.print(" ");
+	            }
+	            System.out.print("    ");
+
+	            
+	            //N
+	            for (int j = 0; j < n; j++) {
+	                if (j==0||j==n-1||i==j) 
+	                System.out.print("*");
+	                
+	                else
+	                System.out.print(" ");
+	            }
+	            System.out.print("    ");
+
+	            System.out.println("");
+	               
+	        }
+	    
+	    }
+
+}
